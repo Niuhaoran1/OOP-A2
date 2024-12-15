534d498c16e71643776c94d16ea67b09bbd80b55
Niuhaoran1 <162036361+Niuhaoran1@users.noreply.github.com>
2024-12-15 18:05:40 +0800

    第三次更改

--- Ride.java
+++ Ride.java
@@ -3,29 +3,40 @@ public class Ride {
     private String rideName;
     private boolean isOpen;
     private Employee rideOperator;
-
+ 
     // 默认构造函数
     public Ride() {
     }
-
+ 
     // 带参数的构造函数
     public Ride(String rideName, boolean isOpen, Employee rideOperator) {
         this.rideName = rideName;
         this.isOpen = isOpen;
         this.rideOperator = rideOperator;
     }
-
+ 
     // Getter和Setter方法
     public String getRideName() {
         return rideName;
     }
-
+ 
     public void setRideName(String rideName) {
         this.rideName = rideName;
     }
-
-    // ... 其他getter和setter方法
-
+ 
+    public boolean isOpen() {
+        return isOpen;
+    }
+ 
+    public void setOpen(boolean isOpen) {
+        this.isOpen = isOpen;
+    }
+ 
+    // 获取游乐设施操作员的方法
+    public Employee getRideOperator() {
+        return rideOperator;
+    }
+ 
     // 分配员工操作游乐设施的方法
     public void assignRideOperator(Employee operator) {
         this.rideOperator = operator;
