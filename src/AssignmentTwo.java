ff7f3a2bb82c8ea0e075deda497399d564c4c2f3
Niuhaoran1 <162036361+Niuhaoran1@users.noreply.github.com>
2024-12-15 17:22:57 +0800

    第一次更改

---
+++ src/AssignmentTwo.java
@@ -0,0 +1,33 @@
+package src;
+
+public class AssignmentTwo {
+    public static void main(String[] args) {
+        // 这里可以添加测试代码，或者调用其他方法
+    }
+
+    public void partThree() {
+        // 实现第三部分的功能
+    }
+
+    public void partFourA() {
+        // 实现第四部分A的功能
+    }
+
+    public void partFourB() {
+        // 实现第四部分B的功能
+    }
+
+    public void partFive() {
+        // 实现第五部分的功能
+    }
+
+    public void partSix() {
+        // 实现第六部分的功能
+    }
+
+    public void partSeven() {
+        // 实现第七部分的功能
+    }
+}
+    
+
